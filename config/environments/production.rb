3030e5f68c0218f08982f45857d6bfa9403346e3
Yury Voloshin <yvoloshin01@gmail.com>
2015-06-10 18:42:07 +0000

    Lesson 10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Flixster::Application.configure do
+
+  config.action_mailer.default_url_options = { :host => 'flixster-yv.herokuapp.com' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
