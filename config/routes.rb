e322804cd62ea49571984a350998fa73ecdd8941
Yury Voloshin <yvoloshin01@gmail.com>
2015-06-11 16:14:03 +0000

    Lesson 12

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Flixster::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
 
+  namespace :instructor do
+    resources :courses, :only => [:new, :create, :show]
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
