47adffc557798f41fc78d9c43b2a343a4e4f3d98
Yury Voloshin <yvoloshin01@gmail.com>
2015-06-22 00:38:04 +0000

    Lesson 27

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,21 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_course
+
 	def show
 	end
 
 	private
+
 	helper_method :current_lesson
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
+	end
+
+	def require_authorized_for_current_course
+		@course = current_lesson.section.course
+		if current_user.enrolled_in?(@course) == false 
+			redirect_to course_path(@course), :alert => 'You must be enrolled to access this lesson.'
+		end
 	end
 end
