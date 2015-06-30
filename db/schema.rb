40d1b220bc2b5c2128ed9c845d67684b856fcdf4
Yury Voloshin <yvoloshin01@gmail.com>
2015-06-30 17:19:50 +0000

    Lesson 31 error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623141826) do
+ActiveRecord::Schema.define(version: 20150630163007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,9 +53,11 @@ ActiveRecord::Schema.define(version: 20150623141826) do
     t.string   "course_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "row_order"
   end
 
   add_index "sections", ["course_id"], name: "index_sections_on_course_id", using: :btree
+  add_index "sections", ["row_order"], name: "index_sections_on_row_order", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
