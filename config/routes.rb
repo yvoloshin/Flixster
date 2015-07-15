91bdefca20cff1100b2fcbb2f862d321a0e42565
Yury Voloshin <yvoloshin01@gmail.com>
2015-07-15 14:21:29 +0000

    Lesson 34

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Flixster::Application.routes.draw do
       resources :lessons, :only => [:new, :create]
     end
     resources :courses, :only => [:new, :create, :show] do
-      resources :sections, :only => [:new, :create]    
+      resources :sections, :only => [:create]    
     end
   end
 
